53c588e990d5178fbbdd63d4f41cc4298cdc21ce
johnhongannguyen <johhaneshongan@gmail.com>
2022-02-25 02:14:24 -0800

    create header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Header from './components/Header';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+    <Header />
+    <Main>
+      <MainContent>
+      <TodoContent>
+        <Title>Dashboard</Title>
+        <Greeting>Good Morning, John</Greeting>
+        {[<h2>Cooking</h2>, <h2>Reading</h2>]}
+      </TodoContent>
+      </MainContent>
+    </Main>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  background-color: #18181f;
+  min-height: 100vh;
+  min-width: 100 vw;
+  color: #eee;
+`
+
+const Main = styled.div`
+  display: flex;
+`
+const MainContent = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  transition: 0.3s;
+`
+const TodoContent = styled.div`
+  max-width: 700px;
+  width: 100%;
+
+`
+
+const Title = styled.div`
+  margin: 50px 0;
+  font-size: 30px;
+  font-weight: 700;
+`
+
+const Greeting = styled.div`
+  font-size: 36px;
+  font-weight: 800;
+  margin-bottom: 20px;
+
+`
---
+++ src/components/Header.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <Wrapper>
+        <HeaderItem>
+          <i  className='fas fa-bars'/>  
+        </HeaderItem>
+
+        <HeaderItem>
+            <i  className='fas fa-border-all'/>  
+            <span>Dashboard</span>
+        </HeaderItem>
+
+        <HeaderItem>
+            <i  className='fas fa-images'/>
+            <span>Collection </span> 
+        </HeaderItem>
+
+        <PlaceHolder />
+
+        <HeaderItem>
+           
+           <Profile>
+            <img 
+                src='https://news.artnet.com/app/news-upload/2021/09/Yuga-Labs-Bored-Ape-Yacht-Club-4466.jpg'
+                alt=''
+
+            />   
+            </Profile>  
+        </HeaderItem>
+
+</Wrapper>  
+    )
+}
+
+export default Header
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 70px;
+    align-items: center;
+    background-color: #20212d;
+    padding: 0 20px; 
+    /* this is for shadow */ 
+    -webkit-box-shadow: 0px 4px 15px 0px #121212;
+    box-shadow: 0px 4px 15px 0px #121212;
+
+    /* this is for nav bar stick at the top of the page */
+    position: sticky;
+    top: 0;
+`
+
+const Profile = styled.div`
+    img{
+        height: 30px;
+        border-radius: 50%;
+    }
+    display: flex;
+    align-items: center;
+`
+
+const HeaderItem = styled.div`
+    color: #eee;
+
+    /* giving item more room */
+    padding: 10px 16px;
+    border-radius: 4px;
+
+    span {
+        margin-left: 10px;
+        font-weight: 500;
+    }
+
+    &:hover{
+        background-color:#18181f;
+        transition: 0.3s;
+        cursor: pointer;
+    }
+`
+
+const PlaceHolder = styled.div`
+    flex: 1
+`
